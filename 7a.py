6b6c7d959b1fbc47983ab353b2d64dbde9c6f22d
mhavos <matej.haverlik@gmail.com>
2021-12-13 09:26:08 +0100

    week 2

--- 7a.py
+++
@@ -1,9 +0,0 @@
-with open("7in.txt", "r") as file:
-    l = list(map(int, file.readline().split(",")))
-    l.sort()
-    total = 0
-    i = len(l)//2
-    for num in l:
-        total += abs(num - l[i])
-
-    print(total)
