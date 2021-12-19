5d35859fde8a064654dc15ab2b5c2c545e6f28c9
mhavos <matej.haverlik@gmail.com>
2021-12-19 13:12:29 +0100

    week 3

---
+++ week 3/13a.py
@@ -0,0 +1,42 @@
+def fold(dots, text):
+    text = text.strip("\n").strip("fold along ")
+    mode = text[0]
+    num = int(text[2:])
+    if mode == "y":
+        l = len(dots) - 1
+        while l > num:
+            while len(dots[2*num - l]) < len(dots[l]):
+                dots[2*num - l].append(" ")
+            for i in range(len(dots[l])):
+                if dots[l][i] != " ":
+                    dots[2*num - l][i] = dots[l][i]
+            dots.pop()
+            l -= 1
+    else:
+        for row in dots:
+            i = len(row) - 1
+            while i > num:
+                if row[i] != " ":
+                    row[2*num - i] = row[i]
+                row.pop()
+                i -= 1
+
+with open("13in.txt", "r") as file:
+    dots = []
+    for line in file:
+        if line == "\n":
+            break
+        x, y = map(int, line.split(","))
+        while len(dots) <= y:
+            dots.append([])
+        while len(dots[y]) <= x:
+            dots[y].append(" ")
+        dots[y][x] = "#"
+    line = file.readline()
+    fold(dots, line)
+    total = 0
+    for row in dots:
+        for char in row:
+            if char != " ":
+                total += 1
+    print(total)
