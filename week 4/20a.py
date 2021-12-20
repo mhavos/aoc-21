87fb7eb31170312c63c5af0d21dbbbc8d49dde87
mhavos <matej.haverlik@gmail.com>
2021-12-20 10:15:53 +0100

    december 20

---
+++ week 4/20a.py
@@ -0,0 +1,30 @@
+im = []
+with open("20in.txt", "r") as file:
+    update = [x for x in file.readline()[:-1]]
+    file.readline()
+    for line in file:
+        im.append([x for x in line[:-1]])
+
+for i in range(2):
+    newim = []
+    for r in range(-1, len(im)+1):
+        newim.append([])
+        for s in range(-1, len(im[0])+1):
+            num = ""
+            for rr in (-1, 0, 1):
+                if not (0 <= r + rr < len(im)):
+                    num += "111" if (update[0]=="1" and i%2) else "000"
+                    continue
+                for ss in (-1, 0, 1):
+                    if not (0 <= s + ss < len(im[0])):
+                        num += "1" if (update[0]=="1" and i%2) else "0"
+                        continue
+                    num += im[r+rr][s+ss]
+            newim[-1].append(update[int(num, 2)])
+    im = newim
+
+total = 0
+for row in im:
+    for cell in row:
+        total += int(cell)
+print(total)
