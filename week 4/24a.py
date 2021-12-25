7e7fbf2c1b1cc0ff4d23c2e4e9a9fcc82184dabe
mhavos <matej.haverlik@gmail.com>
2021-12-25 13:38:29 +0100

    december 24 (not really, it's in a google sheet)

--- week 4/24a.py
+++ week 4/24a.py
@@ -1,37 +1,15 @@
-class Expression:
-    def __init__(self, value=0, index=None):
-        self.value = {None:value}
-        if index is not None:
-            self.value[index] = 1
-    def __repr__(self):
-        return f"{' + '.join([repr(self.value[key]) for key in self.value])}"
-    def copy(self):
-        new = Expression()
-        new.value = self.value.copy()
-        return new
-
-    def __iadd__(self, other):
-        if type(other) == int:
-            self.value[None] += other
-            return
-
-        for key in other.value:
-            if self.value.get(key, None) is None:
-                self.value[key] = other.value[key]
-            else:
-                self.value[key] += other.value[key]
-
-    def __imul__(self, other):
-        if type(other) == int:
-            if other == 0:
-                self.value = {None:0}
-                return
-            for key in self.value:
-                self.value[key] *= other
-            return
-
-        for key in 
-
 with open("24in.txt", "r") as file:
-    for line in file:
-        pass
+    code = file.readlines()
+    ps = [[int(code[19*i + line][:-1].split()[-1]) for line in (4, 5, 15)] for i in range(14)]
+
+def check(num):
+    z = []
+    for i in range(14):
+        w = num[i]
+        p = ps[i]
+
+        x = z[-1] + p[1]
+        if p[0] == 26:
+            z.pop()
+
+        if x + p[1]
